ece2ba1505c62e6e564c210342c8f7d8e8f670f9
gqbrriela <gabriela.campos@escolar.ifrn.edu.br>
2022-06-11 01:30:14 -0300

    att

--- viveiro/wsgi.py
+++ viveiro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projeto01 project.
+WSGI config for viveiro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viveiro.settings')
 
 application = get_wsgi_application()
