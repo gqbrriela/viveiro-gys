c6652e943ad2ab2b9902ea9167c9f40d716086f2
gqbrriela <gabriela.campos@escolar.ifrn.edu.br>
2022-07-11 17:15:17 -0300

    att ''

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomeView, ContatosView, ProdutosView
 
 urlpatterns = [
-    path('home/', HomeView.as_view()),
+    path('', HomeView.as_view()),
     path('contatos/', ContatosView.as_view()),
     path('produtos/', ProdutosView.as_view()),
 
