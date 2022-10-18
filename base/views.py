91472d3a2fd365e8ea5c37bb572c04520dda9244
gabriela <81991464+gqbrriela@users.noreply.github.com>
2022-10-18 15:35:23 -0300

    att css

--- base/views.py
+++ base/views.py
@@ -16,7 +16,7 @@ class CartView(TemplateView):
 
 def register(request):
     if request.method == 'GET':
-        return render(request, "base/pages/register.html")
+        return render(request, "base/pages/cadastro.html")
     
     else:
         username = request.POST.get('username')
