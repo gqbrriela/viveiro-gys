036bf4ab228273272a0f904fa357b92c10085b6e
gabriela <81991464+gqbrriela@users.noreply.github.com>
2022-10-25 17:32:13 -0300

    Update models.py

--- base/models.py
+++ base/models.py
@@ -5,5 +5,4 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    endereço = models.CharField(max_length=30)
-    
+    endereço = models.CharField(max_length=35)
