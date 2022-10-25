00e83e6eb6e925aa857c981a3895c964ef007200
gabriela <81991464+gqbrriela@users.noreply.github.com>
2022-10-25 17:18:54 -0300

    att models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    endereço = models.CharField(max_length=30)
+    
