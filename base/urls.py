15f90c48ab47db3a197d1825aad28ebc5fd3fcae
gqbrriela <gabriela.campos@escolar.ifrn.edu.br>
2022-11-22 22:13:50 -0300

    22/11 att

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import HomeView, ContatosView, ProdutosView, CartView, register
 
 
-
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('contatos/', ContatosView.as_view(), name="contatos"),
--- base/views.py
+++ base/views.py
@@ -6,17 +6,17 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 class HomeView(TemplateView):
-    template_name = 'base/pages/home.html'
+    template_name = 'home.html'
 class ContatosView(TemplateView):
-    template_name = 'base/pages/contatos.html'
+    template_name = 'contatos.html'
 class ProdutosView(TemplateView):
-    template_name = 'base/pages/produtos.html'
+    template_name = 'produtos.html'
 class CartView(TemplateView):
-    template_name = 'base/pages/cart.html'
+    template_name = 'cart.html'
 
 def register(request):
     if request.method == 'GET':
-        return render(request, "base/pages/cadastro.html")
+        return render(request, "cadastro.html")
     
     else:
         username = request.POST.get('username')
@@ -30,7 +30,7 @@ def register(request):
         #else:
         user = User.objects.create_user(username=username, email=email, password=password)
         
-        return render(request, "base/pages/home.html")
+        return render(request, "home.html")
 
 
 
